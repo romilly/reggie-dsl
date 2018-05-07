d3182c98ab1a33bea5cc474e0bdb36fd5f762315
Romilly Cocking <romilly.cocking@gmail.com>
2018-05-07 17:18:54 +0100

    Modified defaults for <multiple>

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='reggie',
+    version='0.1a1',
+    packages=['reggie', 'examples'],
+    url='https://github.com/romilly/reggie',
+    license='MIT',
+    author='romilly',
+    author_email='romilly.cocking@gmail.com',
+    description='Python DSL for Regular Expressions',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: DSLs',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6', ],
+)
