812664d454699c87da43a4e8a018cd22caa97500
Romilly Cocking <romilly.cocking@gmail.com>
2018-05-07 17:32:47 +0100

    Fixed topic for PyPi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: DSLs',
+        'Topic :: Software Development :: Libraries :: Python Modules',
 
         # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
